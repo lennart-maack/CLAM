0f752915c31b2a20c1c26c211401519ae78682b9
Lennart Maack <lennartma.19@gmail.com>
2023-08-01 18:51:09 +0200

    Adapt .sh scripts

--- run_CLAM.sh
+++ run_CLAM.sh
@@ -1,6 +1,72 @@
 #!/bin/bash
 
-MODEL_NAME=vit_large_patch16_224_MAE_histopatho_pretr
+# MODEL_NAME=vit_large_patch16_224_MAE_histopatho_pretr
+# CUDA_VISIBLE_DEVICES=0 python main.py \
+#     --drop_out \
+#     --lr 2e-4 \
+#     --exp_code CMB_vs_DN \
+#     --weighted_sample \
+#     --bag_loss ce \
+#     --inst_loss svm \
+#     --task CMB_vs_DN \
+#     --model_type clam_sb \
+#     --log_data \
+#     --data_root_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME} \
+#     --results_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME}/training_01 \
+#     --split_dir CMB_vs_DN_train_val_100 \
+#     --max_epochs 180
+
+# MODEL_NAME=vit_large_patch16_384_norm_pretr
+# CUDA_VISIBLE_DEVICES=0 python main.py \
+#     --drop_out \
+#     --lr 2e-4 \
+#     --exp_code CMB_vs_DN \
+#     --weighted_sample \
+#     --bag_loss ce \
+#     --inst_loss svm \
+#     --task CMB_vs_DN \
+#     --model_type clam_sb \
+#     --log_data \
+#     --data_root_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME} \
+#     --results_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME}/training_01 \
+#     --split_dir CMB_vs_DN_train_val_100 \
+#     --max_epochs 180
+
+
+# MODEL_NAME=vit_large_patch16_224_norm_pretr
+# CUDA_VISIBLE_DEVICES=0 python main.py \
+#     --drop_out \
+#     --lr 2e-4 \
+#     --exp_code CMB_vs_DN \
+#     --weighted_sample \
+#     --bag_loss ce \
+#     --inst_loss svm \
+#     --task CMB_vs_DN \
+#     --model_type clam_sb \
+#     --log_data \
+#     --data_root_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME} \
+#     --results_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME}/training_01 \
+#     --split_dir CMB_vs_DN_train_val_100 \
+#     --max_epochs 180
+
+# MODEL_NAME=resnet_50
+# CUDA_VISIBLE_DEVICES=0 python main.py \
+#     --drop_out \
+#     --lr 2e-4 \
+#     --exp_code CMB_vs_DN \
+#     --weighted_sample \
+#     --bag_loss ce \
+#     --inst_loss svm \
+#     --task CMB_vs_DN \
+#     --model_type clam_sb \
+#     --log_data \
+#     --data_root_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME} \
+#     --results_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME}/training_01 \
+#     --split_dir CMB_vs_DN_train_val_100 \
+#     --max_epochs 180
+
+MODEL_NAME=vit_small_patch16_384_HIPT_pretr
+NUM_EPOCHS=2
 CUDA_VISIBLE_DEVICES=0 python main.py \
     --drop_out \
     --lr 2e-4 \
@@ -9,14 +75,17 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
     --bag_loss ce \
     --inst_loss svm \
     --task CMB_vs_DN \
-    --model_type clam_sb \
     --log_data \
     --data_root_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME} \
-    --results_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME}/training_01 \
+    --results_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME}/training_epochs_${NUM_EPOCHS} \
     --split_dir CMB_vs_DN_train_val_100 \
-    --max_epochs 180
+    --max_epochs ${NUM_EPOCHS} \
+    --model_type clam_sb_ViT_small_384 \
+    --seed 42
+
 
 MODEL_NAME=vit_large_patch16_384_norm_pretr
+NUM_EPOCHS=35
 CUDA_VISIBLE_DEVICES=0 python main.py \
     --drop_out \
     --lr 2e-4 \
@@ -25,42 +94,10 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
     --bag_loss ce \
     --inst_loss svm \
     --task CMB_vs_DN \
-    --model_type clam_sb \
     --log_data \
     --data_root_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME} \
-    --results_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME}/training_01 \
+    --results_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME}/training_epochs_${NUM_EPOCHS} \
     --split_dir CMB_vs_DN_train_val_100 \
-    --max_epochs 180
-
-
-MODEL_NAME=vit_large_patch16_224_norm_pretr
-CUDA_VISIBLE_DEVICES=0 python main.py \
-    --drop_out \
-    --lr 2e-4 \
-    --exp_code CMB_vs_DN \
-    --weighted_sample \
-    --bag_loss ce \
-    --inst_loss svm \
-    --task CMB_vs_DN \
+    --max_epochs ${NUM_EPOCHS} \
     --model_type clam_sb \
-    --log_data \
-    --data_root_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME} \
-    --results_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME}/training_01 \
-    --split_dir CMB_vs_DN_train_val_100 \
-    --max_epochs 180
-
-MODEL_NAME=resnet_50
-CUDA_VISIBLE_DEVICES=0 python main.py \
-    --drop_out \
-    --lr 2e-4 \
-    --exp_code CMB_vs_DN \
-    --weighted_sample \
-    --bag_loss ce \
-    --inst_loss svm \
-    --task CMB_vs_DN \
-    --model_type clam_sb \
-    --log_data \
-    --data_root_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME} \
-    --results_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME}/training_01 \
-    --split_dir CMB_vs_DN_train_val_100 \
-    --max_epochs 180
+    --seed 42
--- run_feature_extraction.sh
+++ run_feature_extraction.sh
@@ -22,7 +22,18 @@
 #     --model ${MODEL_NAME}
 
 
-MODEL_NAME=resnet_50_histo_pretr
+# MODEL_NAME=vit_small_patch16_384_HIPT_pretr
+# CUDA_VISIBLE_DEVICES=0 python extract_features_fp.py \
+#     --data_h5_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME} \
+#     --data_slide_dir /data/Maack/PANT/ndpi_reduced/NDPI \
+#     --csv_path /data/Maack/PANT/CLAM/experiments/${MODEL_NAME}/process_list_autogen.csv \
+#     --feat_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME} \
+#     --batch_size 512 \
+#     --slide_ext .ndpi \
+#     --model ${MODEL_NAME} \
+
+
+MODEL_NAME=resnet_18_sshisto
 CUDA_VISIBLE_DEVICES=0 python extract_features_fp.py \
     --data_h5_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME} \
     --data_slide_dir /data/Maack/PANT/ndpi_reduced/NDPI \
@@ -30,4 +41,14 @@ CUDA_VISIBLE_DEVICES=0 python extract_features_fp.py \
     --feat_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME} \
     --batch_size 1024 \
     --slide_ext .ndpi \
-    --model ${MODEL_NAME} \
+    --model ${MODEL_NAME}
+
+MODEL_NAME=resnet_18
+CUDA_VISIBLE_DEVICES=0 python extract_features_fp.py \
+    --data_h5_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME} \
+    --data_slide_dir /data/Maack/PANT/ndpi_reduced/NDPI \
+    --csv_path /data/Maack/PANT/CLAM/experiments/${MODEL_NAME}/process_list_autogen.csv \
+    --feat_dir /data/Maack/PANT/CLAM/experiments/${MODEL_NAME} \
+    --batch_size 1024 \
+    --slide_ext .ndpi \
+    --model ${MODEL_NAME}
